32ef6f5032d1a19fbc13b9f4d312d92d3ff6e311
panchenkoav <panchenko.a.v@gmail.com>
2018-07-03 18:08:25 +0300

    new classes added

---
+++ ConsoleApp2/Task4/Class_Doctor(Lecture_example).cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+
+namespace Task
+
+{
+    public class Doctor
+    {
+        private string name;
+        private double salary = 100;
+        private int expYear;
+        public static string Title = "Doctor";
+        public const int Month = 12;
+        public readonly int MyNumber = 7;
+
+        public Doctor(string n, double s, int exp)
+        {
+            name = n;
+            salary = s;
+
+
+
+
+            expYear = exp;
+        }
+
+        public Doctor(string n)
+        {
+            name = n;
+        }
+
+        public Doctor()
+        {
+
+        }
+
+        public string Name
+        {
+            set
+            {
+                name = value;
+            }
+            get
+            {
+                return name;
+            }
+
+        }
+
+        public double Salary
+        {
+            set
+            {
+                salary = value;
+            }
+            get
+            {
+                return salary;
+            }
+
+        }
+
+        public int ExpYear
+        {
+            set
+            {
+                expYear = value;
+            }
+            get
+            {
+                return expYear;
+            }
+        }
+        public void Output()
+        {
+            Console.WriteLine("Doctor{0} with experience {1} and salary {2}", name, expYear, salary);
+        }
+
+        public double SalaryChange(double many)
+        {
+            salary += many;
+            return salary;
+        }
+        public static bool operator ==(Doctor first, Doctor second)
+        {
+            return first.name == second.name;
+        }
+        public static bool operator !=(Doctor first, Doctor second)
+        {
+            return first.name != second.name;
+        }
+        public static explicit operator Doctor(string newName)
+        {
+            Doctor temp = new Doctor();
+            temp.name = newName;
+            return temp;
+        }
+
+        public static implicit operator string(Doctor doc)
+        {
+            return doc.ToString();
+        }
+    }
+}
---
+++ ConsoleApp2/Task4/Task4_class_Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    public class Car
+    {
+        private string name;
+        private string color;
+        private int price;
+        public const string companyName = "SS";
+
+        public Car()
+        {
+
+        }
+        public Car(string c, int p)
+        {
+            color = c;
+            price = p;
+        }
+
+        public string Color
+        {
+            set
+            {
+                color = value;
+            }
+            get
+            {
+                return color;
+            }
+        }
+        public void Input()
+        {
+            Console.WriteLine("Enter the name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the color");
+            color = Console.ReadLine();
+            Console.WriteLine("Enter the price");
+            price = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Car name is {0} and color is {1}. It's price is {2}", name, color, price);
+        }
+        public int ChangePrice(int x)
+        {
+            price += price * x / 100;
+            return price;
+        }
+    }
+}
